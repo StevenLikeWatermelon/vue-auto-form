96fb9466e9cf2e8592df74726439f5d4de784b8f
shenwenjie <15805199474@163.com>
2021-02-08 09:40:38 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app1">
+      <form-create v-model="fApi" :rule="rule" :option="option"></form-create>
+       <ElButton type="primary" plain @click="submit">提交</ElButton>
+        <ElButton aligen="center" plain @click="reset">重置</ElButton>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      // 实例对象
+      fApi: {},
+      // 表单生成规则
+      rule: [
+        {
+          type: 'input',
+          field: 'goods_name',
+          title: '名称'
+        },
+        {
+          type: 'datePicker',
+          field: 'created_at',
+          title: '时间'
+        },
+        {
+          type: 'radio',
+          title: '是否展示input',
+          field: ' is_postage',
+          value: 0,
+          options: [
+            { value: 0, label: '不展示', disabled: false },
+            { value: 1, label: '展示', disabled: false }
+          ],
+          control: [
+            {
+              value: 1,
+              rule: [
+                {
+                  type: 'input',
+                  field: 'postage_money',
+                  title: '动态展示的',
+                  value: 0
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      // 组件参数配置
+      option: {
+        // 表单提交事件
+        submitBtn: false
+      }
+    }
+  },
+  methods: {
+    submit () {
+      this.fApi.submit((formData, $f) => {
+        alert(JSON.stringify(formData))
+      })
+    },
+    reset () {
+      this.fApi.resetFields()
+    }
   }
 }
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import store from './store'
+import formCreate from '@form-create/element-ui'
 
+Vue.use(ElementUI)
+Vue.use(formCreate)
 Vue.config.productionTip = false
 
-new Vue({
+window.app = new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')

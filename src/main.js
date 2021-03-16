337fce7441d96a178b6576c62cd61d10a9cdb6cb
shenwenjie <15805199474@163.com>
2021-03-16 09:39:48 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import XlViews, { XlSelectTree } from 'xl-views'
 import 'xl-views/dist/xl-views.css'
 
 import App from './App.vue'
-import locationMap from './components/mapForm.vue'
+import locationMap from 'xl-views/lib/components/mapForm/mapForm.vue'
 import store from './store'
 import formCreate from 'xl-form-create'
+// 部分组件为了提交考虑需要手动引入
+
+// 地图组件引入时 。需要注意要在 index.html中提前 引入 srcipt 地图脚本
+
 formCreate.component('locationMap', locationMap)
 formCreate.component('selectTree', XlSelectTree)
 
